ed8a7278cc955ed0bfc65e5aa319ce62b017cdd4
Carter Cobb <carter.cobb72@gmail.com>
2022-08-23 17:49:19 -0600

    styled pages

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { fallback } from "../components/SuspenseUI";
+import ReactJson from "@textea/json-viewer";
 
 const NotFoundPage = () => {
   const { user, logout, isLoading, isAuthenticated } = useAuth0();
@@ -10,10 +11,19 @@ const NotFoundPage = () => {
   if (!isAuthenticated) return <Navigate to="/" />;
 
   return (
-    <div>
-      <h1>Account Page</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <button onClick={logout}>Logout</button>
+    <div className="auth-main">
+      <h1>
+        Account{" "}
+        <button className="btn right" onClick={logout}>
+          Logout
+        </button>
+      </h1>
+
+      <p>Explore the user object below:</p>
+      <ReactJson src={user} />
+      <div className="footer">
+        &copy; Carter Cobb 2022. All rights reserved.
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,17 @@ import React from "react";
 const NotFoundPage = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <div>
+    <div className="home-main">
       <h1>Home </h1>
+      <p>
+        <b>This is the MFA Lab!!</b>
+        <br />
+        <br />
+        It uses Auth0 to authenticate and keep track of users. to test out the
+        flow, login or create an account:
+      </p>
       <button
+        className="btn"
         onClick={() =>
           loginWithRedirect({
             appState: { returnTo: "/account" },
@@ -15,6 +23,9 @@ const NotFoundPage = () => {
       >
         Login
       </button>
+      <div className="footer">
+        &copy; Carter Cobb 2022. All rights reserved.
+      </div>
     </div>
   );
 };
