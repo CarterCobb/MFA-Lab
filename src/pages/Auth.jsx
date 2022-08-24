7021d47253ce0f2f0184bfb001715de1be824599
Carter Cobb <carter.cobb72@gmail.com>
2022-08-24 11:12:21 -0600

    added access token to page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { fallback } from "../components/SuspenseUI";
@@ -7,11 +7,14 @@ import ReactJson from "@textea/json-viewer";
 const AuthPage = () => {
   const { user, logout, isLoading, isAuthenticated, getAccessTokenSilently } =
     useAuth0();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    getAccessTokenSilently().then(setToken);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (isLoading) return fallback;
   if (!isAuthenticated) return <Navigate to="/" />;
-
-  getAccessTokenSilently().then(console.log);
 
   return (
     <div className="auth-main">
@@ -24,6 +27,8 @@ const AuthPage = () => {
 
       <p>Explore the user object below:</p>
       <ReactJson src={user} />
+      <p>API Access Token (valid for 3 min):</p>
+      <p>{token}</p>
       <div className="footer">
         &copy; Carter Cobb 2022. All rights reserved.
       </div>
